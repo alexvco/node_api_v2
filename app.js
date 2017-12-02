1206d07f60c029382606775f9f7fda1b7b990fec
alexv7 <alexvarjabedian7@gmail.com>
2017-12-02 15:20:53 -0500

    new routes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const api_routes = require('./routes/api');
 
 // set up express app
 const app = express();
+
+app.use('/api/v2', api_routes)
+
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
